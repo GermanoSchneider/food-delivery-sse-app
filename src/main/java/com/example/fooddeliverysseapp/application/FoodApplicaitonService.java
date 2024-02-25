ba9a145221cd7237499bc2570f27acc86ef4fce8
Germano Schneider <germanoschneider87@gmail.com>
2024-02-25 11:43:02 -0300

    added component annotation to the food service class

--- src/main/java/com/example/fooddeliverysseapp/application/FoodApplicaitonService.java
+++ src/main/java/com/example/fooddeliverysseapp/application/FoodApplicaitonService.java
@@ -3,7 +3,9 @@ package com.example.fooddeliverysseapp.application;
 import com.example.fooddeliverysseapp.domain.EventService;
 import com.example.fooddeliverysseapp.domain.Food;
 import com.example.fooddeliverysseapp.domain.FoodService;
+import org.springframework.stereotype.Component;
 
+@Component
 class FoodApplicaitonService implements FoodService {
 
     private final EventService eventService;
